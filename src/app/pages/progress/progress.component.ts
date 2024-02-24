cd5c56eacf9f26fad8e409c22bd962ed9acec57b
Raúl Morales <raulmdaw@gmail.com>
2024-02-24 20:02:41 +0100

    Componente ProgressBar

--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
-  styles: ``
+  styleUrl: './progress.component.css'
 })
 export class ProgressComponent {
 
