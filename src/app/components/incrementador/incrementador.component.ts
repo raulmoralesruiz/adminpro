74b0dac0a72f577b4b14d3c63d539f5d7ce41f49
Raúl Morales <raulmdaw@gmail.com>
2024-02-25 20:58:21 +0100

    @Output - Componente incrementador

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, computed, signal } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-incrementador',
@@ -6,16 +6,20 @@ import { Component, Input, computed, signal } from '@angular/core';
   styles: ``
 })
 export class IncrementadorComponent {
-  // @Input('valor') progreso: number = 50;
-  @Input() progreso: number = 50;
+  @Input('valor') progreso: number = 50;
+
+  @Output('valor') valorSalida: EventEmitter<number> = new EventEmitter();
 
   cambiarValor(valor: number) {
     if (this.progreso >= 100 && valor >= 0) {
+      this.valorSalida.emit(100);
       this.progreso = 100;
     } else if (this.progreso <= 0 && valor < 0) {
+      this.valorSalida.emit(0);
       this.progreso = 0;
     } else {
       this.progreso = this.progreso + valor;
+      this.valorSalida.emit(this.progreso);
     }
   }
 }
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -6,5 +6,14 @@ import { Component, OnInit, computed, signal } from '@angular/core';
   styleUrl: './progress.component.css'
 })
 export class ProgressComponent {
+  progreso1: number = 25;
+  progreso2: number = 35;
 
+  get obtenerProgreso1() {
+    return `${this.progreso1}%`;
+  }
+
+  get obtenerProgreso2() {
+    return `${this.progreso2}%`;
+  }
 }
