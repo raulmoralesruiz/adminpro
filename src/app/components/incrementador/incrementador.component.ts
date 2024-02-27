a6c9e3a79193cb3af6fbb6a956483cfa8054c40e
Raúl Morales <raulmdaw@gmail.com>
2024-02-27 18:43:04 +0100

    Color de los botones de forma condicional

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -1,14 +1,19 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-incrementador',
   templateUrl: './incrementador.component.html',
   styles: ``
 })
-export class IncrementadorComponent {
+export class IncrementadorComponent implements OnInit {
   @Input('valor') progreso: number = 50;
+  @Input() btnClass: string = 'btn-primary';
 
   @Output('valor') valorSalida: EventEmitter<number> = new EventEmitter();
+
+  ngOnInit(): void {
+    this.btnClass = `btn ${this.btnClass}`
+  }
 
   cambiarValor(valor: number) {
     if (this.progreso >= 100 && valor >= 0) {
