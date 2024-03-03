47a278e4d41ae3360d42a2e9fb507fd44006c44b
Raúl Morales <raulmdaw@gmail.com>
2024-03-03 14:45:51 +0100

    Diseño inicial de la página account-settings

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -10,6 +10,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
 import { ProgressComponent } from './progress/progress.component';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { ProgressComponent } from './progress/progress.component';
     ProgressComponent,
     Grafica1Component,
     PagesComponent,
+    AccountSettingsComponent,
   ],
   imports: [
     RouterModule,
@@ -30,6 +32,7 @@ import { ProgressComponent } from './progress/progress.component';
     ProgressComponent,
     Grafica1Component,
     PagesComponent,
+    AccountSettingsComponent,
   ]
 })
 export class PagesModule { }
